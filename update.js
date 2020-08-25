68b634fe1ace95477043304c5ad73e9837149d06
dusan-milanez <dusan.milanez@yahoo.com>
2020-08-25 08:10:41 +0200

    Adding our Serverless API

---
+++ update.js
@@ -0,0 +1,34 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        // 'Key' defines the partition key and sort key of the item to be updated
+        // - 'userId': Identity Pool identity id of the authenticated user
+        // - 'noteId': path parameter
+        Key: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            radniNalogId: event.pathParameters.id
+        },
+        // 'UpdateExpression' defines the attributes to be updated
+        // 'ExpressionAttributeValues' defines the value in the update expression
+        UpdateExpression: "SET klijent = :klijent, kontakt = :kontakt, uredjaj = :uredjaj, napomena = :napomena, attachment = :attachment",
+        ExpressionAttributeValues: {
+            ":attachment": data.attachment || null,
+            ":napomena": data.napomena || null,
+            ":uredjaj": data.uredjaj || null,
+            ":kontakt": data.kontakt || null,
+            ":klijent": data.klijent || null
+        },
+        // 'ReturnValues' specifies if and how to return the item's attributes,
+        // where ALL_NEW returns all attributes of the item after the update; you
+        // can inspect 'result' below to see how it works with different settings
+        ReturnValues: "ALL_NEW"
+    };
+
+    await dynamoDb.update(params);
+
+    return { status: true };
+});
